3ea8cc8335efb66dfd300ca24c4e1d984e4c6443
c1trus <ryanu9@163.com>
2026-02-11 16:22:50 +0800

    style(图谱): 移除图谱标题文本

--- src/plugin/features/graph-view.ts
+++ src/plugin/features/graph-view.ts
@@ -12,7 +12,7 @@ export class GraphView implements FeatureGenerator
 
 		const graphHeader = graphWrapper.createDiv("feature-header");
 		const graphTitle = graphHeader.createDiv("feature-title");
-		graphTitle.innerText = "Interactive Graph";
+		graphTitle.innerText = "";
 
 		const graphEl = graphWrapper.createDiv("graph-view-placeholder");
 		const expandSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg>`;
