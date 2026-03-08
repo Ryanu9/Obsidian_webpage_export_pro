34014861e637cdc34718bd3b597e41ba105aff0c
c1trus <ryanu9@163.com>
2026-03-08 21:15:26 +0800

    perf(搜索框)：优化加密文章的预览显示

--- src/frontend/main/modal-search.ts
+++ src/frontend/main/modal-search.ts
@@ -444,6 +444,7 @@ export class ModalSearch {
 		previewInner.classList.add('preview-inner', 'markdown-preview-view', 'markdown-rendered');
 		innerDiv.forEach(child => previewInner.appendChild(child.cloneNode(true)));
 		this.previewContainer.replaceChildren(previewInner);
+		this.previewContainer.scrollTop = 0;
 
 		this.rebindCopyButtons(previewInner);
 
